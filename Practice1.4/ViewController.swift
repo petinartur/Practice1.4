17fd2027f2dd9b38f2adb8c91549975c6cfb2a90
Artur <petin.art@gmail.com>
2021-10-19 17:30:34 +0300

    Little fix

--- Practice1.4/ViewController.swift
+++ Practice1.4/ViewController.swift
@@ -9,11 +9,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var viewColor: UIView!
+    
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var bleuLabel: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        super.viewDidLoad()//
+        viewColor.layer.cornerRadius = 10
+        
+        updateColor()
+        
+        redLabel.text = String(format: "%.2f", redSlider.value)
+        greenLabel.text = String(format: "%.2f", greenSlider.value)
+        bleuLabel.text = String(format: "%.2f", blueSlider.value)
     }
 
-
+    @IBAction func redSliderActiv() {
+        updateColor()
+        redLabel.text = String(format: "%.2f", redSlider.value)
+    }
+    
+    @IBAction func greenSliderActiv() {
+        updateColor()
+        greenLabel.text = String(format: "%.2f", greenSlider.value)
+    }
+    @IBAction func blueSliderActiv() {
+        updateColor()
+        bleuLabel.text = String(format: "%.2f", blueSlider.value)
+    }
+    
+    private func updateColor(){
+        let red = CGFloat(redSlider.value)
+        let green = CGFloat(greenSlider.value)
+        let blue = CGFloat(blueSlider.value)
+        viewColor.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        
+    }
 }
-
